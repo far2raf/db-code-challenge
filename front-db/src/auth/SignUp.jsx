847a237097b58100093b2e03da61be437d3e6c5d
avdeev1 <avdeev.1van@icloud.com>
2020-08-21 13:13:24 +0100

    Fix auth

--- front-db/src/auth/SignUp.jsx
+++ front-db/src/auth/SignUp.jsx
@@ -9,23 +9,18 @@ export default class SignUp extends Component {
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
-
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
-
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <label>Login</label>
+                    <input type="text" className="form-control" placeholder="Login" />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
                     <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group">
+                    <label>Repeat password</label>
+                    <input type="password" className="form-control" placeholder="Repeat password" />
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
