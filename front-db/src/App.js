203f2acd9b3ba8a3dba855de28cc762e77220cc4
sophiekh <sophieee.kh@gmail.com>
2020-08-21 14:21:39 +0100

    dashboard-auth routing

--- front-db/src/App.js
+++ front-db/src/App.js
@@ -3,38 +3,22 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import SignUp from './auth/SignUp';
-import Login from './auth/Login';
+
+import Dashboard from './Dashboard';
+import Auth from './auth/Auth';
 
 
 function App() {
-  return (<Router>
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Auth} />
+        <Route path="/sign-in" component={Auth} />
+        <Route path="/sign-up" component={Auth} />
+        <Route path="/dashboard" component={Dashboard}></Route>
+      </Switch>
 
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-          </Switch>
-        </div>
-      </div>
-    </div></Router>
+    </Router>
   );
 }
 
---
+++ front-db/src/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Report from './Report';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Report title='Average buy/sell prices per instrument' />
+        </div>
+    );
+};
+
+export default Dashboard;
