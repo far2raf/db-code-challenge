22f91d8b9620d6f637e68d34e9f667f4f7aaba8a
avdeev1 <avdeev.1van@icloud.com>
2020-08-21 13:55:06 +0100

    Added auth (hardcode)

--- front-db/src/auth/Login.jsx
+++ front-db/src/auth/Login.jsx
@@ -4,20 +4,44 @@ const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    const onsubmit = (evt) => {
+        evt.preventDefault();
+        if (username !== 'admin' || password !== 'admin') {
+            alert('Invalid username or password!')
+        }
+
+        localStorage.setItem('token', '12345');
+    }
+
     return(
         <form>
             <h3>Sign in</h3>
             <div className='form-group'>
                 <label>Login</label>
-                <input type='text' className='form-control' 
-                    placeholder='Enter your login'></input>
+                <input type='text'
+                    required
+                    className='form-control'
+                    onChange={evt => setUsername(evt.target.value)}
+                    placeholder='Enter your login'
+                    value={username} 
+                />
             </div>
             <div className='form-group'>
                 <label>Password</label>
-                <input type='password' className='form-control' 
-                    placeholder='Enter your password'></input>
+                <input
+                    required
+                    type='password'
+                    className='form-control' 
+                    placeholder='Enter your password'
+                    value={password}
+                    onChange={evt => setPassword(evt.target.value)}
+                />
             </div>
-            <button type='submit' className='btn btn-primary btn-block'>Submit</button>
+            <button
+                type='submit'
+                className='btn btn-primary btn-block'
+                onClick={evt => onsubmit(evt)}
+            >Submit</button>
         </form>
     );
 };
