be7233f0d2f11431c20852d1805c7277f50358f7
sophiekh <sophieee.kh@gmail.com>
2020-08-25 11:41:11 +0100

    fix bar chart

--- front-db/src/components/Chart/BarChart.jsx
+++ front-db/src/components/Chart/BarChart.jsx
@@ -1,13 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import DatetimeRangePicker from 'react-datetime-range-picker';
 import { Bar } from 'react-chartjs-2';
 import './../Dashboard/Dashboard.css';
 
+const sampleData = [
+    { "instrument": "Galactia", "buy": 4444, "sell": 4200 },
+    { "instrument": "Astronomica", "buy": 3400, "sell": 3200 },
+    { "instrument": "Lunatic", "buy": 1800, "sell": 1600 },
+    { "instrument": "Eclipse", "buy": 9800, "sell": 9300 },
+    { "instrument": "Heliosphere", "buy": 7800, "sell": 7200 },
+];
 
 const BarChart = (props) => {
     const [dateRange, setDateRange] = useState({end: new Date(), start: new Date()});
+    const [data, setData] = useState(sampleData);
+    const [dataForChart, setDataForChart] = useState({});
     const title = props.title;
-    const data = props.data;
     const dateFormat = (date) => {
         let partOfDay = (date.getHours() > 11) ? 'PM' : 'AM';
         let hour;
@@ -26,18 +34,25 @@ const BarChart = (props) => {
         console.log(dateRange);
         setDateRange(dateRange);
     };
-    const dataForChart = {
-        labels: data.map(item => item.instrument),
-        datasets: [{
-            label: 'Average buy',
-            data: data.map(item => item.buy),
-            backgroundColor: 'rgb(255, 99, 132)'
-        }, {
-            label: 'Average sell',
-            data: data.map(item => item.sell),
-            backgroundColor: 'rgb(0, 128, 255)'
-        }]
+    useEffect(() => {
+        //add getting data
+        setDataForChart(prepareData(data));
+    }, [data]);
+    const prepareData = (data) => {
+        return {
+            labels: data.map(item => item.instrument),
+            datasets: [{
+                label: 'Average buy',
+                data: data.map(item => item.buy),
+                backgroundColor: 'rgb(255, 99, 132)'
+            }, {
+                label: 'Average sell',
+                data: data.map(item => item.sell),
+                backgroundColor: 'rgb(0, 128, 255)'
+            }]
+        };
     };
+    
     return (
         <div className='container'>
             <div>
